f9c5ece261cee356021890edf266519094563f2a
bigcompanystore <156739040+bigcompanystore@users.noreply.github.com>
2024-01-26 22:35:16 +0700

    Add files via upload

---
+++ asset/list.js
@@ -0,0 +1,45 @@
+const products = [
+  {
+    image:"asset/meuble1.jpg" ,
+    title: "Futuristic Meuble" ,
+    info: "My Meuble with artistic theme build with heart and full of functionality"  ,
+},
+ {
+  image:"asset/meuble2.jpg" ,
+    title: "My Beautiful Meuble" ,
+    info: "The scene and the beautiful landscape will force the creativity to build the meuble to the limit"  ,
+},
+  {
+ image:"asset/meuble3.jpg" ,
+    title: "Embracing Leisure" ,
+    info: "A place to rest with dominated on natural and beautyfull set up"  ,
+  },
+
+  {
+ image:"asset/meuble4.jpg" ,
+    title: "Natural Meuble" ,
+    info: "Nice place to do working and relaxing on the same time"  ,
+  },
+   {
+    image:"asset/meuble1.jpg" ,
+    title: "Futuristic Meuble" ,
+    info: "My Meuble with artistic theme build with heart and full of functionality"  ,
+},
+ {
+  image:"asset/meuble2.jpg" ,
+    title: "My Beautiful Meuble" ,
+    info: "The scene and the beautiful landscape will force the creativity to build the meuble to the limit"  ,
+},
+  {
+    image:"asset/meuble1.jpg" ,
+    title: "Futuristic Meuble" ,
+    info: "My Meuble with artistic theme build with heart and full of functionality"  ,
+},
+ {
+  image:"asset/meuble2.jpg" ,
+    title: "My Beautiful Meuble" ,
+    info: "The scene and the beautiful landscape will force the creativity to build the meuble to the limit"  ,
+}
+
+
+  ]
