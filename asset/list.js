76ae65f37c09d1922c9fca633b9589c67296155b
bigcompanystore <156739040+bigcompanystore@users.noreply.github.com>
2024-01-31 18:45:19 +0700

    Update list.js

--- asset/list.js
+++ asset/list.js
@@ -1,13 +1,13 @@
 const products = [
-  {
-    image:"asset/meuble..1.jpg" ,
-    title: "chairs" ,
-    info: "various kinds of chairs"  ,
-},
-  {
+ {
   image:"asset/kulit.2.jpg" ,
     title: "Cowhide wallet, handicraft" ,
     info: "Homemade products made from cowhide and manually sewn by hand"  ,
+},
+    {
+    image:"asset/meuble..1.jpg" ,
+    title: "chairs" ,
+    info: "various kinds of chairs"  ,
 },
  {
     image:"asset/meuble.1.jpg" ,
