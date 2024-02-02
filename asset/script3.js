ef20100e14e92bec8d9069aa9531401bd6cb677b
bigcompanystore <156739040+bigcompanystore@users.noreply.github.com>
2024-02-02 22:36:32 +0700

    Create script3.js

---
+++ asset/script3.js
@@ -0,0 +1,26 @@
+const productsEl = document.querySelector(".products");
+
+
+function renderProducts() {
+  products.forEach((product) => {
+    
+    productsEl.innerHTML += 
+      `
+           
+                   <article class="item">
+            <img src="${product.image}" alt="meuble">
+      <div class="sub-item">
+       <h3>"${product.title}"</h3>
+            <p>"${product.info}"</p>
+      </div>
+            
+       <button type="button" class="button tombol2"><a href=""${product.link}""><b>Show</b></a>
+</button>     
+</article>
+        `
+      
+      ;
+  });
+  
+}
+renderProducts();
