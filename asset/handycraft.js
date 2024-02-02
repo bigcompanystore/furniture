b1a01ee6f05ac8ddf45362ed48f41d3dedb5144f
bigcompanystore <156739040+bigcompanystore@users.noreply.github.com>
2024-02-02 20:39:18 +0700

    Update handycraft.js

--- asset/handycraft.js
+++ asset/handycraft.js
@@ -1,1 +1,80 @@
+const products = [
+  {
+    image:"asset/kulit.5.jpg" ,
+    title: "The door handles are covered in leather" ,
+    info: "Homemade products made from cowhide and manually sewn by hand"  ,
+},   
+  {
+    image:"asset/kulit.4.jpg" ,
+    title: "leather name tag" ,
+    info: "Homemade products made from cowhide and manually sewn by hand"  ,
+},
+ {
+  image:"asset/kulit.2.jpg" ,
+    title: "Cowhide wallet, handicraft" ,
+    info: "Homemade products made from cowhide and manually sewn by hand"  ,
+},
+    {
+    image:"asset/meuble..1.jpg" ,
+    title: "chairs" ,
+    info: "various kinds of chairs"  ,
+},
+ {
+    image:"asset/meuble.1.jpg" ,
+    title: "Futuristic Meuble" ,
+    info: "My Meuble with artistic theme build with heart and full of functionality"  ,
+},
+ {
+  image:"asset/meuble.2.jpg" ,
+    title: "My Beautiful Meuble" ,
+    info: "The scene and the beautiful landscape will force the creativity to build the meuble to the limit"  ,
+},
+  {
+ image:"asset/meuble.3.jpg" ,
+    title: "Buy Teak Wood Sofa Set Online | TeakLab" ,
+    info: "A sofa set is a truly regal piece of furniture for your home. Finely crafted from the best quality teak wood, its fineness is simply out of this world"  ,
+  },
+  {
+ image:"asset/meuble.4.jpg" ,
+    title: "fideliohaus — truebluemeandyou: Japanese Table and Chairs in a" ,
+    info: "fideliohaus — truebluemeandyou: Japanese Table and Chairs in a"  ,
+  },
+   {
+    image:"asset/meuble.5.jpg" ,
+    title: "Crafting Dreams from Timber: Unlock 16,000 Plans" ,
+    info: "Experience the satisfaction of crafting functional and beautiful pieces using TedsWoodworking's foolproof plans."  ,
+},
+ {
+  image:"asset/meuble.6.jpg" ,
+    title: "Round Dining Table & Chairs for Small Homes" ,
+    info: "Not all of us are lucky to have a huge home with enough space for fancy chairs and dining tables. This space saving dining table works for 4 people but"  ,
+},
+  {
+    image:"asset/meuble.7.jpg" ,
+    title: "Wonder Walls: Unique Home Masterpieces" ,
+    info: "unique home decor unique home decor ideas unique home decorating"  ,
+},
+ {
+  image:"asset/meuble.8.jpg" ,
+    title: "Arditi Collection Sled Coffee Table Wood in Gray | 13.7 H x 39.5 W x 39.5 D" ,
+    info: "Arditi Collection is one of the exclusive designers and producers"  ,
+},
+  {
+  image:"asset/meuble.9.jpg" ,
+    title: "Moscow Table" ,
+    info: "Glam form an essential function combine to create this must"  ,
+},
+  {
+  image:"asset/meuble.10.jpg" ,
+    title: "Cabinet 01" ,
+    info: "An iconic design from Vincent’s archives, revisited."  ,
+},
+  {
+  image:"asset/meuble.11.jpg" ,
+    title: "Dumont Armoire - 72\" - Solid Cherry - Teak Finish - Mid Century Modern Inspired" ,
+    info: "Product Details, Stunning armoire built entirely in solid wood"  ,
+},
 
+
+
+  ]
