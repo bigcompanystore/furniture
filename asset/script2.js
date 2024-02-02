e18aecbe22a08ed3a9fde7a8635e3a2e0068aa29
bigcompanystore <156739040+bigcompanystore@users.noreply.github.com>
2024-02-02 20:54:29 +0700

    Update script2.js

--- asset/script2.js
+++ asset/script2.js
@@ -27,4 +27,35 @@ function renderProducts() {
     
   
 }
-renderProducts();
+renderHandycrafts();
+
+const handycraftEl = document.querySelector(".handycrafts");
+
+handycraftShow = handycrafts.slice(0, 6);
+
+function renderHandycraft() {
+    
+  handycraftShow.forEach((handycraft) => {
+    
+    handycraftEl.innerHTML += 
+      `
+           
+                   <article class="item">
+            <img src="${handycraft.image}" alt="handycraft">
+      <div class= sub-item>
+       <h3>${handycraft.title}</h3>
+            <p>${handycraft.info}</p>
+      </div>
+          <button type="button" class="btn btn-info"><a href="${handycraft.link}"><b>Show</b></a>
+</button> 
+
+          
+</article>
+        `
+      
+      ;
+  });
+    
+  
+}
+renderHandycrafts();
