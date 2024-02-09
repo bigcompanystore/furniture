dc80b6684f3718b11e9ff998537a6bf4bc9dc311
bigcompanystore <156739040+bigcompanystore@users.noreply.github.com>
2024-02-10 01:56:06 +0700

    Update handycraft.js

--- asset/handycraft.js
+++ asset/handycraft.js
@@ -1,4 +1,9 @@
 const handycrafts = [
+ {
+    image:"asset/kulit.5.jpg" ,
+    title: "The door handles are covered in leather" ,
+    info: "Homemade products made from cowhide and manually sewn by hand"  ,
+}, 
   {
     image:"asset/kulit.5.jpg" ,
     title: "The door handles are covered in leather" ,
