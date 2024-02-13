4663a405d130b58ce59e43628c4084fae6747a80
bigcompanystore <156739040+bigcompanystore@users.noreply.github.com>
2024-02-13 19:00:34 +0700

    Update handycraft.js

--- asset/handycraft.js
+++ asset/handycraft.js
@@ -5,6 +5,11 @@ const handycrafts = [
     info: "Homemade products made from cowhide and manually sewn by hand"  ,
 },
  {
+    image:"asset/kulit.24.jpg" ,
+    title: "leather wallet" ,
+    info: "Homemade products made from cowhide and manually sewn by hand"  ,
+},
+ {
     image:"asset/kulit.25.jpg" ,
     title: "leather knife case" ,
     info: "Homemade products made from cowhide and manually sewn by hand"  ,
