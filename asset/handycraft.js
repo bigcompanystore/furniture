4a1d0b7faae0aaa78f95e660c95a36bf564effbb
bigcompanystore <156739040+bigcompanystore@users.noreply.github.com>
2024-02-13 19:04:04 +0700

    Update handycraft.js

--- asset/handycraft.js
+++ asset/handycraft.js
@@ -3,6 +3,11 @@ const handycrafts = [
     image:"asset/kulit.27.jpg" ,
     title: "leather smoking pipe case" ,
     info: "Homemade products made from cowhide and manually sewn by hand"  ,
+}
+ {
+    image:"asset/kulit.27.jpg" ,
+    title: "leather smoking pipe case" ,
+    info: "Homemade products made from cowhide and manually sewn by hand"  ,
 },
  {
     image:"asset/kulit.26.jpg" ,
