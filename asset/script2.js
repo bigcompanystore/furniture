47436cacde522e680e1c8f84edad3bb8a63e28f7
nouveaupo <77199339+nouveaupo@users.noreply.github.com>
2024-02-14 12:40:04 +0700

    Update script2.js

--- asset/script2.js
+++ asset/script2.js
@@ -70,7 +70,7 @@ function renderGambar() {
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById(${product.ide});
+var img = document.getElementById(${product.ide}));
 var modalImg = document.getElementById(${product.spot});
 var captionText = document.getElementById("caption");
 img.onclick = function(){
