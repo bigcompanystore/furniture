a8b827c445cb31049d75f8ad410176cb3ff9771c
nouveaupo <77199339+nouveaupo@users.noreply.github.com>
2024-02-14 12:46:35 +0700

    Update script2.js

--- asset/script2.js
+++ asset/script2.js
@@ -109,7 +109,7 @@ var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg");
-var modalImg = document.getElementById(${product.spot});
+var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
   modal.style.display = "block";
