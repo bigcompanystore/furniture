10758ebe1cf28957a16fe91366d5fc942ef2fb65
nouveaupo <77199339+nouveaupo@users.noreply.github.com>
2024-02-14 07:33:17 +0700

    Update list.js

--- asset/list.js
+++ asset/list.js
@@ -4,7 +4,7 @@ const products = [
  image:"asset/meuble.3.jpg" ,
     title: "Buy Teak Wood Sofa Set Online | TeakLab" ,
     info: "A sofa set is a truly regal piece of furniture for your home. Finely crafted from the best quality teak wood, its fineness is simply out of this world"  ,
-   ide: myImg,
+   ide: "myImg",
   },
   {
  image:"asset/meuble.4.jpg" ,
