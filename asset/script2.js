9ec743be53dfbdc709c9107d43d2f8ed02060a6f
nouveaupo <77199339+nouveaupo@users.noreply.github.com>
2024-02-14 07:45:24 +0700

    Update script2.js

--- asset/script2.js
+++ asset/script2.js
@@ -91,5 +91,25 @@ span.onclick = function() {
 
 
 
+// Get the modal 2
+var modal = document.getElementById("myModal2");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg2");
+var modalImg = document.getElementById("img012");
+var captionText = document.getElementById("caption2");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("close2")[0];
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() { 
+  modal.style.display = "none";
+}
 
 
