4a3141617381c3e250216a571ae95fcf56f4ecc7
nouveaupo <77199339+nouveaupo@users.noreply.github.com>
2024-02-14 12:44:55 +0700

    Update script2.js

--- asset/script2.js
+++ asset/script2.js
@@ -108,7 +108,7 @@ renderHandycraft();
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById(${product.ide});
+var img = document.getElementById("myImg);
 var modalImg = document.getElementById(${product.spot});
 var captionText = document.getElementById("caption");
 img.onclick = function(){
