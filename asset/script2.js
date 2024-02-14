f0d766dedfac980b43a5f05d00c9147a3a68ed83
nouveaupo <77199339+nouveaupo@users.noreply.github.com>
2024-02-14 13:42:50 +0700

    Update script2.js

--- asset/script2.js
+++ asset/script2.js
@@ -109,8 +109,8 @@ var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+//var modalImg = document.getElementById("img01");
+//var captionText = document.getElementById("caption");
 img.onclick = function(){
   modal.style.display = "block";
 //  modalImg.src = "${product.image}" ;
@@ -134,7 +134,7 @@ var modal2 = document.getElementById("myModal2");
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img2 = document.getElementById("myImg2");
 //var modalImg2 = document.getElementById("img012");
-var captionText2 = document.getElementById("caption2");
+//var captionText2 = document.getElementById("caption2");
 img2.onclick = function(){
   modal2.style.display = "block";
 //  modalImg2.src = "asset/meuble.4.jpg";
