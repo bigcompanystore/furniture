0083c8f0593d96a986a20997d10f325db1fcee2e
nouveaupo <77199339+nouveaupo@users.noreply.github.com>
2024-02-14 07:14:34 +0700

    Update script.js

--- asset/script.js
+++ asset/script.js
@@ -8,15 +8,10 @@ function renderProducts() {
       `
            
                    <article class="item">
-            <img id="myImg" src="${product.image}" alt="Snow">
+            <img src="${product.image}" alt="Snow">
 
             
-            <!-- The Modal -->
-<div id="myModal" class="modal">
-  <span class="close">&times;</span>
-  <img class="modal-content" id="img01">
-  <div id="caption"></div>
-</div>
+          
 
 
             
@@ -36,23 +31,3 @@ function renderProducts() {
 }
 renderProducts();
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
