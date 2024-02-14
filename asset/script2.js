626157f990ac4f1571033e121070b733193866aa
nouveaupo <77199339+nouveaupo@users.noreply.github.com>
2024-02-14 12:41:57 +0700

    Update script2.js

--- asset/script2.js
+++ asset/script2.js
@@ -66,26 +66,7 @@ function renderGambar() {
       
   });
 
-    // Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById(${product.ide}));
-var modalImg = document.getElementById(${product.spot});
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = ${product.image} ;
-  captionText.innerHTML = "Teak Wood Sofa";
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
     
   
 }
@@ -123,7 +104,26 @@ function renderHandycraft() {
 }
 renderHandycraft();
 
+    // Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById(${product.ide}));
+var modalImg = document.getElementById(${product.spot});
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = ${product.image} ;
+  captionText.innerHTML = "Teak Wood Sofa";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
 
 
 
