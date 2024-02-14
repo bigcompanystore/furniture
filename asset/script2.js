f66ec94456a32b431d859ebb88835db8829b36f5
nouveaupo <77199339+nouveaupo@users.noreply.github.com>
2024-02-14 08:52:43 +0700

    Update script2.js

--- asset/script2.js
+++ asset/script2.js
@@ -22,7 +22,7 @@ function renderProducts() {
        <h3>${product.title}</h3>
             <p>${product.info}</p>
       </div>
-          <button id="${product.ide}" type="button" class="btn tombol2" alt="meuble" src:"${product.image}" alt="meuble">Show</a>
+          <button id="${product.ide}" type="button" class="btn tombol2" alt="meuble" src="${product.image}" alt="meuble">Show
 </button> 
 
           
