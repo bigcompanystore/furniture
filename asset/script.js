ad5daf0095e272617bacb6b97d3f275bc203e436
nouveaupo <77199339+nouveaupo@users.noreply.github.com>
2024-02-18 21:33:37 +0700

    Update script.js

--- asset/script.js
+++ asset/script.js
@@ -20,8 +20,8 @@ function renderProducts() {
             <p>"${product.info}"</p>
       </div>
             
-       <button type="button" class="btn btn-info"><a href=""${product.link}"">Show</a>
-</button>     
+       <button id="${product.ide}" type="button" class="btn tombol2" alt="meuble" } alt="meuble">Show
+</button>      
 </article>
         `
       
@@ -31,3 +31,56 @@ function renderProducts() {
 }
 renderProducts();
 
+
+
+function renderGambar() {
+    
+  products.forEach((product) => {
+    
+   gambarEl.innerHTML += 
+      `
+           
+                   <div id="${product.modal}" class="modal">
+  <span class="${product.close}">&times;</span>
+  <img class="modal-content" id="${product.spot}" src="${product.image}">
+  <div id="caption">${product.caption}</div>
+</div>
+        `
+      
+      ;
+
+
+
+      
+  });
+
+
+    
+  
+}
+renderGambar();
+
+
+
+
+
+    // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+//var modalImg = document.getElementById("img01");
+//var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+//  modalImg.src = "${product.image}" ;
+//  captionText.innerHTML = "Teak Wood Sofa";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
