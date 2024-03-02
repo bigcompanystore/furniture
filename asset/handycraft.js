87f79ae89ba1c6c4c08a0b8aacfb4f44f0f73fec
bigcompanystore <156739040+bigcompanystore@users.noreply.github.com>
2024-03-02 19:42:11 +0700

    Update handycraft.js

--- asset/handycraft.js
+++ asset/handycraft.js
@@ -1,5 +1,15 @@
 const handycrafts = [
  {
+    image:"asset/kulit.35.jpg" ,
+    title: "leather bottle hanger" ,
+    info: "leather bottle hanger, minimalist and cool"  ,
+   ide: "myImgb",
+   
+   close: "closeb",
+   modal: "myModalb",
+   caption: "leather knife case belt",
+},
+ {
     image:"asset/kulit.34.jpg" ,
     title: "leather drink bottle holder" ,
     info: "leather drink bottle holder, to hang on the belt"  ,
